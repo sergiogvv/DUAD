d07c2592940b328218d28a0417e8d1b845e0582f
Sergio Velasquez <sergiogvv@gmail.com>
2025-07-26 12:09:40 -0400

    prueba con ejercicio 1

--- Semana 12/Ejercicio 1.py
+++ Semana 12/Ejercicio 1.py
@@ -5,7 +5,6 @@ class BankAccount():
         self.balance += money
         print(f'\nDeposito de {money}\nSu nuevo balance es: {self.balance}')
 
-
     def withdraw_money(self, money):
         self.balance -= money
         print(f'\nRetiro de {money}\nSu nuevo balance es: {self.balance}')
@@ -32,18 +31,9 @@ class SavingsAccount(BankAccount):
             print(f'\nRetiro de {money}\nSu nuevo balance es: {self.balance}\n')
 
 
+a = BankAccount()
+b = BankAccount()
+a.deposit_money(100)
+print(b.balance) # Hubiese esperado ver el balance como 100 si fuera un problema...
 
-my_savings = SavingsAccount(1000) #Nueva cuenta creada, balance inicial: 1000
-my_other_savings = SavingsAccount(5000) #Nueva cuenta creada, balance inicial: 5000
-
-my_savings.print_balance() #Su balance es: 1000
-
-my_other_savings.print_balance() #Su balance es: 5000
-
-
-my_savings.deposit_money(4000) # 1000 + 4000: Deposito de 4000, Su nuevo balance es: 5000
-my_savings.withdraw_money(5000) # 5000 - 5000: Transacción inválida: No se puede retirar dinero por debajo del balance minimo
-my_savings.withdraw_money(500) # 5000 - 500: Retiro de 500, Su nuevo balance es: 4500
-
-my_other_savings.print_balance() #Su balance es: 5000
 
