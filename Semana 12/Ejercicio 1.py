a4751fad8ae600599b875720b78ccf5c9026249a
Sergio Velasquez <sergiogvv@gmail.com>
2025-07-26 09:22:48 -0400

    Revision de Ejercicio 1, agregado Ejercicio 3

--- Semana 12/Ejercicio 1.py
+++ Semana 12/Ejercicio 1.py
@@ -3,12 +3,15 @@ class BankAccount():
 
     def deposit_money(self, money):
         self.balance += money
-        print(f'Deposito de {money}\nSu nuevo balance es: {self.balance}\n')
+        print(f'\nDeposito de {money}\nSu nuevo balance es: {self.balance}')
 
 
     def withdraw_money(self, money):
         self.balance -= money
-        print(f'Retiro de {money}\nSu nuevo balance es: {self.balance}\n')
+        print(f'\nRetiro de {money}\nSu nuevo balance es: {self.balance}')
+
+    def print_balance(self):
+        print(f'\nSu balance es: {self.balance}')
 
 
 class SavingsAccount(BankAccount):
@@ -16,25 +19,31 @@ class SavingsAccount(BankAccount):
     def __init__(self, min_balance,):
         self.min_balance = min_balance
         self.balance = min_balance
-        print(f'Nueva cuenta creada, balance inicial: {min_balance}\n')
+        print(f'\nNueva cuenta creada, balance inicial: {min_balance}')
         
 
     def withdraw_money(self, money):
         self.balance -= money
         if self.balance < self.min_balance:
-            print('Transacción inválida: No se puede retirar dinero por debajo del balance minimo')
+            print('\nTransacción inválida: No se puede retirar dinero por debajo del balance minimo')
             self.balance += money 
             print(f'Su balance sigue siendo {self.balance}\n')
         else:
-            print(f'Retiro de {money}\nSu nuevo balance es: {self.balance}\n')
+            print(f'\nRetiro de {money}\nSu nuevo balance es: {self.balance}\n')
 
 
 
-my_savings = SavingsAccount(1000)
+my_savings = SavingsAccount(1000) #Nueva cuenta creada, balance inicial: 1000
+my_other_savings = SavingsAccount(5000) #Nueva cuenta creada, balance inicial: 5000
 
-my_savings.deposit_money(4000)
+my_savings.print_balance() #Su balance es: 1000
 
-my_savings.withdraw_money(5000)
+my_other_savings.print_balance() #Su balance es: 5000
 
-my_savings.withdraw_money(500)
+
+my_savings.deposit_money(4000) # 1000 + 4000: Deposito de 4000, Su nuevo balance es: 5000
+my_savings.withdraw_money(5000) # 5000 - 5000: Transacción inválida: No se puede retirar dinero por debajo del balance minimo
+my_savings.withdraw_money(500) # 5000 - 500: Retiro de 500, Su nuevo balance es: 4500
+
+my_other_savings.print_balance() #Su balance es: 5000
 
