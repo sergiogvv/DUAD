cd389630b4e57a63a60b387d4c9ed78f4d078acd
Sergio Velasquez <sergiogvv@gmail.com>
2025-08-12 16:04:38 -0400

    Se agregan decoradores para mejorar claridad del codigo

--- Semana 15/Ejercicios de Algoritmos de Ordenamiento/Ejercicio_1.py
+++ Semana 15/Ejercicios de Algoritmos de Ordenamiento/Ejercicio_1.py
@@ -11,46 +11,54 @@
 #         - Luego aplique `bubble_sort` si pasa las validaciones
 #         - Si hay error, debe lanzar un mensaje apropiado
 
+def validate_numbers(func):
+    def wrapper(list_of_numbers):
+            for element in list_of_numbers:
+                if isinstance(element, int) == False and isinstance(element, float) == False :
+                    raise TypeError('Error: La lista contiene elementos no numéricos')
+            func(list_of_numbers) # Primero se hace la validacion, despues se ejecuta la func(*args)
+    return wrapper
 
+def validate_non_empty_list(func):
+    def wrapper(arg):
+        if not arg:
+            raise ValueError('Error: La lista esta vacia')
+        func(arg)
+    return wrapper
+
+
+@validate_non_empty_list
+@validate_numbers
 def bubble_sort(unsorted_list):
-    try:
-        if not unsorted_list:
-            raise ValueError
-        for element in unsorted_list:
-            if not str(element).isdigit():
-                raise TypeError
-        flippings = 0
-        iteration = 0
-        for j in range(0,len(unsorted_list)-1):
-            flipped = False
-            
-            for i in range(0,len(unsorted_list)-1-j):
-                current = unsorted_list[i]
-                next = unsorted_list[i+1]
-            
-                if current > next:
-                    unsorted_list[i+1] = current
-                    unsorted_list[i] = next
-                    flipped = True
-                    flippings += 1
-            if flipped == False:
-                print(f'Lista ordenada: {unsorted_list}')
-                print(f'Iteraciones: {iteration}')
-                print(f'Intercambios: {flippings}')
-                return
-            iteration += 1
-        print(f'Lista ordenada: {unsorted_list}')
-        print(f'Iteraciones: {iteration}')
-        print(f'Intercambios: {flippings}')
-    except ValueError:
-        print("Error: La lista esta vacia")
-    except TypeError:
-        print("Error: La lista contiene elementos no numéricos")
+    flippings = 0
+    iteration = 0
+    for j in range(0,len(unsorted_list)-1):
+        flipped = False
+        
+        for i in range(0,len(unsorted_list)-1-j):
+            current = unsorted_list[i]
+            next = unsorted_list[i+1]
+        
+            if current > next:
+                unsorted_list[i+1] = current
+                unsorted_list[i] = next
+                flipped = True
+                flippings += 1
+        if flipped == False:
+            print(f'Lista ordenada: {unsorted_list}')
+            print(f'Iteraciones: {iteration}')
+            print(f'Intercambios: {flippings}')
+            return
+        iteration += 1
+    print(f'Lista ordenada: {unsorted_list}')
+    print(f'Iteraciones: {iteration}')
+    print(f'Intercambios: {flippings}')
+
 
 
 # my_test_list = []
 # my_test_list = ['cinco', 4,'tres', 2, 'uno']
-my_test_list = [5,4,3,2,1]
+my_test_list = [5, 4, 3, 2, 1]
 bubble_sort(my_test_list)
 
 
