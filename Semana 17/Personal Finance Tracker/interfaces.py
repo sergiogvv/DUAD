0a612f081dcc230e4db0f0ec32150908863d8267
Sergio Velasquez <sergiogvv@gmail.com>
2025-08-29 18:34:33 -0400

    Programa funcional sin persistencia

--- Semana 17/Personal Finance Tracker/interfaces.py
+++ Semana 17/Personal Finance Tracker/interfaces.py
@@ -1,6 +1,6 @@
 import FreeSimpleGUI as sg
-from logic import Transaction, Category, FinanceTracker
-from datetime import date
+from logic import Transaction, Category, FinanceTracker, validate_transaction_fields, validate_category_fields, Category_Collection, list_transaction_row_w_color
+from datetime import date, datetime
 
 def name(name):
         return sg.Text(name + '    ', justification='r',size= (20,1),pad=(10,10))
@@ -10,15 +10,17 @@ sg.theme('Default1')
 
 
 def main_window(finance_tracker):
+        category_collection = Category_Collection()
+        
         layout = [
                 [sg.T('PERSONAL FINANCE TRACKER', font='_ 14', justification='c', expand_x=True)],
                 [sg.Text()],
                 [sg.Text("TRANSACTIONS")],
                 [sg.Text()],
-                [sg.CalendarButton('From',target='-FROM-',tooltip=' Select date ',format= '%Y-%m-%d ' ), sg.Input(key='-FROM-', s=10),sg.CalendarButton('To',target='-TO-',tooltip=' Select date ',format= '%Y-%m-%d ' ),sg.Input(key='-TO-',s=10),sg.Push(),sg.Button('Apply Filter',key='-FILTER-')],
-                [sg.Table(finance_tracker.return_transactions(), ['Date', 'Title', 'Amount', 'Category', 'Type'],
+                [sg.CalendarButton('From',target='-FROM-',tooltip=' Select date ',format= '%m/%d/%Y'), sg.Input(key='-FROM-', s=10),sg.CalendarButton('To',target='-TO-',tooltip=' Select date ',format= '%m/%d/%Y' ),sg.Input(key='-TO-',s=10),sg.Push(),sg.Button('Apply Filter',key='-FILTER-')],
+                [sg.Table(finance_tracker.return_transactions(), ['Date', 'Title', 'Amount', 'Category', 'Type'],visible_column_map = ['Date', 'Title', 'Amount', 'Category', 'Type'],
                         def_col_width = 15,
-                        starting_row_number = 1,
+                        starting_row_number = 0,
                         max_col_width = 100,
                         col_widths = [10, 25, 15, 25, 10],
                         vertical_scroll_only = False,
@@ -26,7 +28,7 @@ def main_window(finance_tracker):
                         display_row_numbers = True,
                         justification='right',
                         num_rows=20,
-                        alternating_row_color='lightblue',
+                        #alternating_row_color='lightblue',
                         key='-TABLE-',
                         tooltip='This is a table')],
                 [sg.Push(),sg.Button("Input Expense",key='-EXPENSE-'), sg.Button("Input Income", key='-INCOME-'), sg.Button('Add Category', key='-CATEGORY-'), sg. Button('Export to CSV', key='-CSV-'),sg.Push()],
@@ -42,28 +44,36 @@ def main_window(finance_tracker):
                 if event == sg.WIN_CLOSED:
                         break
                 if event == '-EXPENSE-':
-                        expense = transaction_window('Expense')
+                        if  category_collection.category_dict == {}:
+                                sg.popup_error('Please enter a category first before entering a transaction') 
+                                continue                       
+                        expense = transaction_window('Expense', category_collection.return_list_of_category_names())
                         if expense is not None:
                                 finance_tracker.add_transaction(expense)
-                                window['-TABLE-'].update(values=finance_tracker.return_transactions())
+                                window['-TABLE-'].update(values=finance_tracker.return_transactions(),row_colors = list_transaction_row_w_color(finance_tracker.return_transactions(),category_collection))
                 if event == '-INCOME-':
-                        income = transaction_window('Income')
+                        if  category_collection.category_dict == {}:
+                                sg.popup_error('Please enter a category first before entering a transaction')
+                                continue
+                        income = transaction_window('Income', category_collection.return_list_of_category_names())
                         if income is not None:
                                 finance_tracker.add_transaction(income)
-                                window['-TABLE-'].update(values=finance_tracker.return_transactions())
+                                window['-TABLE-'].update(values=finance_tracker.return_transactions(),row_colors = list_transaction_row_w_color(finance_tracker.return_transactions(),category_collection))
                 if event == '-CATEGORY-':
                         category = category_window()
+                        if category is not None:
+                                category_collection.add_category(category)
                 if event == '-CSV-' :
-                        pass
+                        export_to_CSV_window()
 
         window.close()    
 
-def transaction_window(type_title):
+def transaction_window(type_title,category_list):
         layout = [
                 [name('Transaction Name'),sg.Input(key='-NAME-', s=15)],
                 [name('Amount'), sg.Input(key='-AMOUNT-', enable_events=True, s=15)],
-                [name('Category'), sg.Combo(sg.theme_list(), default_value=sg.theme(), s=(15,22), readonly=True, k='-CATEGORY-')],
-                [name('Date (mm/dd/yyyy)'), sg.Input(key='-DATE-', s=15, default_text='08/25/2025'),sg.Text(), sg.CalendarButton('Select date',target='-DATE-',format= '%m/%d/%Y' )],
+                [name('Category'), sg.Combo(category_list, s=(15,22), readonly=True, k='-CATEGORY-')],
+                [name('Date (mm/dd/yyyy)'), sg.Input(key='-DATE-', s=15, default_text=date.today().strftime("%m/%d/%Y")),sg.Text(), sg.CalendarButton('Select date',target='-DATE-',format= '%m/%d/%Y' )],
                 [sg.Text()],
                 [sg.Push(), sg.Button("Save Transaction"),sg.Button("Clear All"), sg.Button("Cancel"),sg.Push()]
                 ] 
@@ -76,8 +86,6 @@ def transaction_window(type_title):
                 if event == 'Cancel':
                         window.close()
                         return None
-
-                # 
                 if event == '-AMOUNT-' and values['-AMOUNT-']:
                         try:
                                 in_as_float = float(values['-AMOUNT-'])
@@ -86,8 +94,13 @@ def transaction_window(type_title):
                                         continue
                                 window['-AMOUNT-'].update(values['-AMOUNT-'][:-1])
                 if event == "Save Transaction":
-                        window.close()
-                        return Transaction(values['-DATE-'], values['-NAME-'], values['-AMOUNT-'], values['-CATEGORY-'], type_title)
+                        valid_fields = validate_transaction_fields(values['-DATE-'], values['-NAME-'], values['-AMOUNT-'], values['-CATEGORY-'])
+                        if  valid_fields == True:
+                                transaction = Transaction(values['-DATE-'], values['-NAME-'], values['-AMOUNT-'], values['-CATEGORY-'], type_title)
+                                window.close()
+                                return transaction
+                        else:
+                                sg.popup_error(valid_fields)
                 if event == 'Clear All':
                         window['-NAME-'].update('')
                         window['-AMOUNT-'].update('')
@@ -107,11 +120,16 @@ def category_window():
         while True:
                 event, values = window.read()
                 print(event,values)
-                if event == sg.WIN_CLOSED or event == 'Cancel':
+                if event == 'Cancel':
                         break
                 if event == "Save Category":
-                        window.close()
-                        return Category(values['-NAME-'], values['-COLOR-'])
+                        valid_fields = validate_category_fields(values['-NAME-'], values['-COLOR-'])
+                        if valid_fields == True:
+                                category = Category(values['-NAME-'], values['-COLOR-'])
+                                window.close()
+                                return category
+                        else:
+                                sg.popup_error(valid_fields)
                 if event == 'Clear All':
                         window['-NAME-'].update('')
                         window['-COLOR-'].update('')
@@ -119,8 +137,23 @@ def category_window():
 
 def export_to_CSV_window():
         layout = [
-                [name('Export Path:'),sg.Input(key='-USER FOLDER-'), sg.FolderBrowse(target='-USER FOLDER-')]
-        ]
+                [name('Export Path'), sg.Input(key='-USER FOLDER-'), sg.FolderBrowse(target='-USER FOLDER-')],
+                [sg.Text()],
+                [sg.Push(), sg.Button("OK"),sg.Button("Clear All"), sg.Button("Cancel"),sg.Push()]                
+                ]
+        
+        window = sg.Window('Export to CSV', layout, disable_close = True, disable_minimize = True, modal = True )
+
+        while True:
+                event, values = window.read()
+                print(event,values)
+                if event == 'Cancel':
+                        break
+                if event == "OK":
+                        pass
+                if event == 'Clear All':
+                        window['-USER FOLDER-'].update('')
+        window.close()  
 
 
 
--- Semana 17/Personal Finance Tracker/logic.py
+++ Semana 17/Personal Finance Tracker/logic.py
@@ -1,30 +1,87 @@
-from datetime import date
+from datetime import date, datetime
 from os import path
 
-class Transaction():
-    def __init__(self, date, title, amount, category, transaction_type):
-        self.date = date
+
+
+def validate_transaction_fields(date_transaction, title, amount,category_name): #validar formulario de transacciones
+    try:
+        if title.strip() == '':
+            raise ValueError('Transaction Name can not be empty')
+        if amount == '': 
+            raise ValueError('Amount can not be empty')
+        if category_name =='':
+            raise ValueError('Category can not be empty.')
+        if datetime.strptime(date_transaction,"%m/%d/%Y").date() > date.today():
+            raise ValueError('Date cannot be a future date')
+        else:
+            return True
+    except Exception as e:
+        return e
+
+
+def validate_category_fields(category_name,color): #validar categoria y color
+    try:
+        if category_name.strip() == '':
+            raise ValueError('Category Name can not be empty')
+        if color == '':
+            raise ValueError('Color can not be empty')
+        else:
+            return True
+    except Exception as e:
+        return e
+
+
+def list_transaction_row_w_color(transaction_list,category_collection): #producir lista de fila y color
+    returned_tuple = []
+    for i in range(len(transaction_list)):
+        returned_tuple.append((i, category_collection.category_dict[transaction_list[i][3]]) )
+    return returned_tuple
+
+
+class Category(): #categoria y color
+    def __init__(self, category_name,color):
+        self.category_name = category_name
+        self.color = color
+
+
+class Category_Collection(): #Base de datos de categorias
+    category_dict = {}
+   
+    def add_category(self, category):
+        self.category_dict[category.category_name]= category.color
+
+    def return_list_of_category_names(self):
+        list_of_category_names = []
+        for category_name in self.category_dict.keys():
+            list_of_category_names.append(category_name)
+        return list_of_category_names
+
+
+class Transaction(): #transaccion
+    date_transaction: date 
+    title: str
+    amount: float
+    category: Category
+    transaction_type: str
+
+
+    def __init__(self, date_transaction, title, amount, category, transaction_type):
+        self.date_transaction = datetime.strptime(date_transaction,"%m/%d/%Y").date()
         self.title  = title
         self.amount = -float(amount) if transaction_type == 'Expense'else float(amount)
         self.category = category
         self.transaction_type = transaction_type
 
 
-class FinanceTracker():
+class FinanceTracker(): #base de datos de transacciones
     transaction_list = []
     def add_transaction(self,transaction):
-        self.transaction_list.append([transaction.date,transaction.title,transaction.amount,transaction.category,transaction.transaction_type])
+        self.transaction_list.append([transaction.date_transaction.strftime("%m/%d/%Y"),transaction.title,transaction.amount,transaction.category,transaction.transaction_type])
 
     def return_transactions(self):
         return self.transaction_list
 
 
-class Category():
-    def __init__(self, category_name,color):
-        self.category_name = category_name
-        self.color = color
 
-
-
-
-
+# transaction_list = []
+# list_transaction_w_color()
