85c116a3970bc30e9a6232f60f32c2388cc01255
Sergio Velasquez <sergiogvv@gmail.com>
2025-09-17 17:30:30 -0400

    API CRUD: Added load_data Exception to errorlog.dat

--- Modulo 2 - Backend/Semana 1/persistence.py
+++ Modulo 2 - Backend/Semana 1/persistence.py
@@ -1,6 +1,6 @@
 from json import (load as jsonload, dump as jsondump, loads as jsonloads)
 from os import path
-
+from utilities import log_exception
 
 def save_data(json_file, data_file_path):
     with open(data_file_path, 'w') as f:
@@ -13,7 +13,7 @@ def load_data(data_file_path):
                 data = jsonload(f)
         return data
     except Exception as ex:
-        print(f'File error: {ex}')
+        log_exception(ex)
         return []
     
     
